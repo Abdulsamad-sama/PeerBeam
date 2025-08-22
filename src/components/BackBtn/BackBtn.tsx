5d3f9284b82db2f3ce6b1ade36688ff74083868d
Abdulsamad hamzat <hamzatabdulsamad34@gmail.com>
2025-08-22 01:39:31 +0100

    Created a socket connection in receiver.tsx

---
+++ src/components/BackBtn/BackBtn.tsx
@@ -0,0 +1,21 @@
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { FaCaretLeft } from "react-icons/fa6";
+
+const BackBtn = () => {
+  const router = useRouter();
+  return (
+    <button
+      type="button"
+      onClick={() => router.back()}
+      className="absolute top-8 left-2 text-black rounded-full bg-gray-200 hover:bg-gray-300 p-2 flex items-center gap-2 shadow"
+      aria-label="Back to Home"
+    >
+      <FaCaretLeft className="text-xl" />
+      <span className="font-semibold">Home</span>
+    </button>
+  );
+};
+
+export default BackBtn;
